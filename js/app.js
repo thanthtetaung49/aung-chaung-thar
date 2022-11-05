e1e0fff38620ccf1a90a3f36222157c658ef61b3
Thant Htet Aung <93987523+thanthtetaung49@users.noreply.github.com>
2022-11-05 20:34:19 +0630

    8:34pm commit

--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
+// Start Jquery Area
+$(document).ready(function () {
+
+    // Start Room Section
+    $(".room-popup").magnificPopup({
+        type: "image"
+    });
+    // End Room Section
+});
+// End Jquery Area
+
+// Start JavaScript Area
 // Start Header
 // Start nav bar
-function dropBtn() {
+function dropBtn(e) {
 
-    let getDropDown = document.getElementById("my-dropdown");
-    getDropDown.classList.toggle("show");
+    // let getDropDown = document.getElementById("my-dropdown");
+    // getDropDown.classList.toggle("show");
+
+    // undefined
+    // console.log(this);
+    // console.log(e);
+
+    // console.log(e.target.nextElementSibling);
+
+    e.target.nextElementSibling.classList.toggle("show");
 };
 
 function dropFilter() {
-    
-    var getSearch, filter, getDropDiv, getLinks,linkValue;
+
+    var getSearch, filter, getDropDiv, getLinks, linkValue;
 
     getSearch = document.getElementById("search");
     filter = getSearch.value.toUpperCase();
-    getDropDiv = document.getElementById("my-dropdown");
-    getLinks = getDropDiv.getElementsByTagName("a");
+    // getDropDiv = document.getElementById("my-dropdown");
+    // getLinks = getDropDiv.getElementsByTagName("a");
+    getLinks = document.querySelectorAll(".my-dropdowns a")
 
     // console.log(getLinks.length);
     // console.log(filter);
@@ -72,3 +93,59 @@ setInterval(autoLoad, 2500);
 // End auto background 
 
 // End Header
+
+// Start Testimonials Section
+const getCompanyName = document.querySelector("company-name");
+const getRole = document.querySelector(".role");
+const getTestimonial = document.querySelector(".testimonial");
+
+const testimonialDatas = [
+    {
+        name: "Rich Star Restaurant",
+        position: "Our Outlet",
+        text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, odit, atque at deleniti cum
+                        unde reiciendis rem libero ullam magni, temporibus distinctio blanditiis autem? Possimus earum
+                        sequi temporibus nam consectetur!`
+    },
+    {
+        name: "Rich Star Restaurant",
+        position: "Our Outlet",
+        text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, odit, atque at deleniti cum
+                        unde reiciendis rem libero ullam magni, temporibus distinctio blanditiis autem? Possimus earum
+                        sequi temporibus nam consectetur!`
+    },
+    {
+        name: "48 Sky Bar Cafe",
+        position: "Our Partner",
+        text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, odit, atque at deleniti cum
+                        unde reiciendis rem libero ullam magni, temporibus distinctio blanditiis autem? Possimus earum
+                        sequi temporibus nam consectetur!`
+    },
+    {
+        name: "Cool Land Swimming Pool",
+        position: "Our Outlet",
+        text: `Lorem ipsum Branch sit, amet consectetur adipisicing elit. Illum, odit, atque at deleniti cum
+                        unde reiciendis rem libero ullam magni, temporibus distinctio blanditiis autem? Possimus earum
+                        sequi temporibus nam consectetur!`
+    },
+    {
+        name: "Chaung Thar SPA",
+        position: "Our Business",
+        text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, odit, atque at deleniti cum
+                        unde reiciendis rem libero ullam magni, temporibus distinctio blanditiis autem? Possimus earum
+                        sequi temporibus nam consectetur!`
+    },
+];
+
+let idx = 0;
+
+function updateTestimonial() {
+
+}
+
+setInterval(updateTestimonial, 10000);
+// End Testimonials Section
+
+// End Javascript Area
+
+// 3TM
